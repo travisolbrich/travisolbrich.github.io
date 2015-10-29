86b2b0b0e7f020e367c21f618641597f797e7c0c
Travis Olbrich <travis.olbrich@gmail.com>
2015-10-29 15:26:17 -0500

    Added basic Canvas map with mouse over events

--- csce444/midterm/web/app/app.js
+++ csce444/midterm/web/app/app.js
@@ -1,9 +1,39 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+
+var content;
+
 angular.module('myApp', [
     'ngRoute',
     'ngSanitize',
     'myApp.view',
     'ui.bootstrap'
-])
+]);
+
+angular.module('myApp.view', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/:test', {
+            controller: 'ViewCtrl'
+        });
+    }])
+
+    .controller('ViewCtrl', ['$scope', '$routeParams', '$location', '$http', '$filter', '$rootScope', function ($scope, $routeParams, $location, $http, $filter, $rootScope) {
+
+
+        $rootScope.$on('$locationChangeSuccess', function (event) {
+            $http.get('content.json').then(function (res) {
+                content = res.data;
+
+                var requested = $location.path().replace('/', '');
+                var single = $filter('filter')(res.data, function (d) {
+                    return d.id == requested;
+                })[0];
+                $scope.all = res.data;
+                $scope.data = single;
+            });
+        });
+
+
+    }]);
---
+++ csce444/midterm/web/app/map.js
@@ -0,0 +1,133 @@
+'use strict';
+
+function drawRow(context, y, color) {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.lineWidth = 20;
+    context.moveTo(0, y);
+    context.lineTo(360, y);
+    context.stroke();
+}
+
+function drawCircle(id, context, x, y, color) {
+    // Start with a white fill to hide the line under the circle
+    context.beginPath();
+    context.arc(x, y, 8, 0, 2 * Math.PI);
+    context.lineWidth = 16;
+    context.strokeStyle = 'white';
+    context.stroke();
+
+    // Add the colored circle
+    context.beginPath();
+    context.arc(x, y, 17, 0, 2 * Math.PI);
+    context.lineWidth = 6;
+    context.strokeStyle = color;
+    context.stroke();
+
+    // Return an array showing a bounding box of the object
+    return {
+        id: id,
+        x1: x - 20,
+        y1: y - 20,
+        x2: x + 20,
+        y2: y + 20,
+        x: x,
+        y: y
+    }
+}
+
+function drawBase() {
+    // Draw rows
+    drawRow(ctx, row1, blue);
+    drawRow(ctx, row2, orange);
+    drawRow(ctx, row3, purple);
+
+    // Draw circles
+    zoneArray.push(drawCircle('recursive-time', ctx, 120, row1, blue));
+
+    zoneArray.push(drawCircle('forking-paths', ctx, 77, row2, orange));
+    zoneArray.push(drawCircle('simultaneous-outcomes', ctx, 160, row2, orange));
+
+    zoneArray.push(drawCircle('remixes', ctx, 35, row3, purple));
+    zoneArray.push(drawCircle('hypertext', ctx, 120, row3, purple));
+    zoneArray.push(drawCircle('synchronization', ctx, 203, row3, purple));
+
+    // Draw text
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "left";
+    ctx.fillText("Recursion", 230, row1 + 6);
+    ctx.fillText("(Non)Linearity", 230, row2 + 6);
+    ctx.fillText("Interconnections", 230, row3 + 6);
+}
+
+function determineZoneHit(mouseX, mouseY){
+    // calculate if the mouse is currently inside the circle
+    for (var i = 0, len = zoneArray.length; i < len; i++) {
+        var inx = (zoneArray[i].x1 < mouseX) && (mouseX < zoneArray[i].x2);
+        var iny = (zoneArray[i].y1 < mouseY) && (mouseY < zoneArray[i].y2);
+        if (inx && iny) {
+            return zoneArray[i];
+        }
+    }
+
+    return null;
+}
+
+function handleMouseMove(e, mouseX, mouseY) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var zone = determineZoneHit(mouseX, mouseY);
+
+    if (zone && !previousHit) {
+        // Display a pointer if we're hovered over a node
+        previousHit = true;
+        var zoneHit = zone;
+
+        document.body.style.cursor = "pointer";
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+
+        ctx.fillText(zoneHit.id, zoneHit.x, zoneHit.y-25);
+    }
+    else if(zone == null && previousHit) {
+        // Restore the base map if not
+        previousHit = false;
+
+        document.body.style.cursor = "default";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawBase();
+    }
+
+}
+
+// Basic canvas defs
+var canvas = document.getElementById('canvas-map');
+var ctx = canvas.getContext('2d');
+var zoneArray = [];
+
+// Colors to use
+var purple = "#8C40A1";
+var orange = "#EBA403";
+var blue = "#6DDCCA";
+
+// Vertical position of the rows in the box
+var row1 = 70;
+var row2 = 136.5;
+var row3 = 203;
+
+var previousHit = false;
+
+drawBase();
+
+// listen for mousemove events
+canvas.addEventListener('mousemove', function (e) {
+        var canvasrect = canvas.getBoundingClientRect();
+        var mouseX = e.clientX - canvasrect.left;
+        var mouseY = e.clientY - canvasrect.top;
+        handleMouseMove(e, mouseX, mouseY);
+    }
+);
